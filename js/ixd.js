aa035e492a562f6f03d9862b86abc4664ab7e1a6
sironij <54326791+sironij@users.noreply.github.com>
2020-09-29 00:18:47 +0200

    Add files via upload

--- js/ixd.js
+++ js/ixd.js
@@ -24,9 +24,7 @@ $("#sidebar-content").hide();
 $("#menu_burger").delay(50).fadeIn();
 }
 
-$("#menu_burger").click(function(){
-  $.fn.openmenu();
-});
+
 
 $("#exit").click(function(){
 $.fn.closemenu();
@@ -211,6 +209,7 @@ function projectsmobile(x) {
     $("#roots").click(function(){
       $("#projects-bar").hide();
       $(".virtual-tour-projects").hide();
+      $("#content").show();
       $(".roots_slide2").show();
       $(".roots_slide3").show();
       $(".roots_slide4").show();
@@ -233,6 +232,7 @@ $("#goprlist").click(function(){
   $("#projects-bar").show();
   $("#menu_burger").show();
   $("#goprlist").hide();
+  $("#content").hide();
 });
 
 
@@ -257,43 +257,6 @@ $("#content").hide();
 $("#roots").click(function(){
   $("#content").show();
 });
-
-
-
-/* TIMER */
-
-// Set the date we're counting down to
-var countDownDate = new Date("Oct 6, 2020 18:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "D " + hours + "H "
-  + minutes + "M " + seconds + "S ";
-
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-
-
-
 
 
 });
